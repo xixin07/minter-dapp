ad90616be541bea3ad3cdb835f0d201f91aa6c6d
xixin07 <55681276+xixin07@users.noreply.github.com>
2022-04-15 01:49:04 +0800

    removed optional contract address

--- backend/src/config.js
+++ backend/src/config.js
@@ -77,7 +77,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = ["0x99b2662c039074F7f79d7eDB28EdcFBC7D50217B"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "0x99b2662c039074F7f79d7eDB28EdcFBC7D50217B"; // If you want to manually include it
+let CONTRACT_ADDRESS = ""; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future

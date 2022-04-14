ebfa434828811c9840d8ebc4d3dfa550fdd9fc30
xixin07 <55681276+xixin07@users.noreply.github.com>
2022-04-15 01:59:46 +0800

    updated public mint start date

--- backend/src/config.js
+++ backend/src/config.js
@@ -66,7 +66,7 @@ const MINT_PRICE = 0.1; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-03-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-04-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PRESALE_MINT_START_DATE = "2022-04-15T01:50:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
